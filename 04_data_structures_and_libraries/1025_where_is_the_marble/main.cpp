4709699cd5d052b389ae3d9ef67abf7e2a6ef398
Nilton <nilton.junior51@hotmail.com>
2019-06-24 11:26:26 -0300

    Update

--- 04_data_structures_and_libraries/1025_where_is_the_marble/main.cpp
+++ 04_data_structures_and_libraries/1025_where_is_the_marble/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-#define MAX_N 10000
+#define MAX_N 10100
 
 int main()
 {
@@ -17,20 +17,22 @@ int main()
         std::cin >> n >> q;
 
         // Break condition
-        if (n == 0 && q == 0) break;
+        if (n == 0 && q == 0) 
+            break;
 
         // Increments test case (for output format only)
         test++;
 
         // Declares and fills the marble array
         unsigned short int marbles[MAX_N];
-        for (unsigned short int iii = 0; iii < n; ++iii) std::cin >> marbles[iii];
+        for (unsigned short int iii = 0; iii < n; ++iii) 
+            std::cin >> marbles[iii];
 
         // Sorts the marbles array
         std::sort(marbles, marbles + n);
 
         // Test case line
-        std::cout << "CASE #" << test << ":\n";
+        std::cout << "CASE# " << test << ":\n";
 
         // Binary search for each query
         for (unsigned short int _ = 0; _ < q; ++_)
@@ -51,18 +53,22 @@ int main()
                 // Redefines mid position
                 mid = (first + last) / 2;
                 // Found the marble
-                if (marbles[mid] == query) index = mid;
+                if (marbles[mid] == query) 
+                    index = mid;
                 // Maybe marble is somewhere before the mid position
-                else if (marbles[mid] > query) last = mid - 1;
+                else if (marbles[mid] > query) 
+                    last = mid - 1;
                 // Marble could be after the mid position
-                else first = mid + 1;
+                else 
+                    first = mid + 1;
 
             }
             if (index == -1) std::cout << query << " not found\n";
             else
             {
                 // In case elements are repeated, verify that the previous one is not equal
-                while (index > 0 && marbles[index - 1] == marbles[index]) index -= 1;
+                while (index > 0 && marbles[index - 1] == marbles[index]) 
+                    index -= 1;
                 std::cout << query << " found at " << index + 1 << "\n";
             }
         }
